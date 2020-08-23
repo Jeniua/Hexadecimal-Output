33b85cf8fc952d9e7ab10dd4636dfedb8fe3d330
Jeniua <64768209+Jeniua@users.noreply.github.com>
2020-08-23 15:35:31 +0300

    Update HexademicalOutput.py

--- HexademicalOutput.py
+++ HexademicalOutput.py
@@ -1,7 +1,6 @@
 def hex_output():
     decnum = 0
     hexnum = input("Enter a hex number to convert: ")
-
     for power, digit in enumerate(reversed(hexnum)):
         print(power,digit,'digit')
         decnum += int(digit) * (16 ** power)
